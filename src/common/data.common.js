d756e8444c992f3214458e376ae5c589646a131d
ngochoang0109 <hoangtn0109@gmail.com>
2022-09-05 11:38:03 +0700

    add menu left bar

--- src/common/data.common.js
+++ src/common/data.common.js
@@ -1,5 +1,8 @@
-export const dataCommon={
-	getDirections:[{id:0,name:'Đông'},{id:0,name:'Tây'},{id:0,name:'Nam'},
-						{id:0,name:'Bắc'},{id:0,name:'Đông Bắc'},{id:0,name:'Tây Bắc'},
-						{id:0,name:'Tây Nam'},{id:0,name:'Đông Nam'}]
+export const dataCommon = {
+	getDirections: [{ id: 0, name: 'Đông' }, { id: 0, name: 'Tây' }, { id: 0, name: 'Nam' },
+	{ id: 0, name: 'Bắc' }, { id: 0, name: 'Đông Bắc' }, { id: 0, name: 'Tây Bắc' },
+	{ id: 0, name: 'Tây Nam' }, { id: 0, name: 'Đông Nam' }],
+	menuBarNewsManagement: [{ className: `item`, title: 'Đăng mới' },
+	{ className: `item`, title: 'Danh sách tin' },
+	{ className: `item`, title: 'Tin nháp' }]
 }
